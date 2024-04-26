09a7df65972bd85d69e833495fc65404cc205317
Parth Kalani <114781255+ParthKalani@users.noreply.github.com>
2024-04-26 06:47:39 -0400

    Major Changes to Maps

--- Streamlit/plugins/crime_data_map.py
+++ Streamlit/plugins/crime_data_map.py
@@ -4,9 +4,11 @@ from .snowflake_operations import fetch_crime_data
 def crime_data_map():
     if "logged_in" in st.session_state and st.session_state.logged_in:
             st.title("Crime Data Map")
-            grouped_df = fetch_crime_data()
-            if grouped_df is not None:
-                st.map(grouped_df.rename(columns={'latitude': 'lat', 'longitude': 'lon', 'size': 'size'}))
+            option = st.selectbox('Select different types of metrics',('Past Week','Past Month','Past Year','All Data'))
+            
+            data = fetch_crime_data(option)
+            if data is not None:
+                st.map(data.rename(columns={'latitude': 'lat', 'longitude': 'lon', 'size': 'size'}))
             else:
                 st.error("No data available.")
             st.markdown("<hr/>", unsafe_allow_html=True)
