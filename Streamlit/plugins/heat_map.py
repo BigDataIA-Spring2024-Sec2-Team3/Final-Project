d76330ce08e62146dbe08e56d2b0dc1d1f64ef6e
Parth Kalani <114781255+ParthKalani@users.noreply.github.com>
2024-04-26 12:06:48 -0400

    Removed extra map

--- Streamlit/plugins/heat_map.py
+++ Streamlit/plugins/heat_map.py
@@ -8,9 +8,7 @@ def heat_map():
     if "logged_in" in st.session_state and st.session_state.logged_in:
         st.title('Heatmaps')
         option = st.selectbox('Select different types of metrics',('Days of Week','Time of Day','Holidays','Year', 'Category'))
-        
         data, elements = fetch_heatmap_crime_data(option)
-
         selection = st.select_slider(f'For the metric: {option}',options=elements)
         filtered_df = data[data['Column0']==selection]
         m = leafmap.Map(center=[37.77, -122.44], zoom=13)
